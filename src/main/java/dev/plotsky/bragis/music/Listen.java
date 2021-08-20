245f4d19468a162ea158f472737b876afecf0d54
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-19 23:19:39 -0700

    Only the most basic of classes
    I just need to show _something_ to the endpoint.

---
+++ src/main/java/dev/plotsky/bragis/music/Listen.java
@@ -0,0 +1,14 @@
+package dev.plotsky.bragis.music;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Listen {
+    @Getter
+    @Setter
+    private String Name;
+}
