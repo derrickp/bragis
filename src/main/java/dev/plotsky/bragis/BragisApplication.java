3d900cb72e191288d9f0cc22bfbe05e268713140
Derrick Plotsky <derrickplotsky@gmail.com>
2021-08-19 23:19:29 -0700

    Initial commit

---
+++ src/main/java/dev/plotsky/bragis/BragisApplication.java
@@ -0,0 +1,13 @@
+package dev.plotsky.bragis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BragisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BragisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/plotsky/bragis/BragisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.plotsky.bragis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BragisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
